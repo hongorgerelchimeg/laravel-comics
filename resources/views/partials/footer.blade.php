b1100572d5f5bcbf7b6af24706c1741e88b3590f
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-04-27 17:14:17 +0200

    blade partials and template created and connection initialized

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,34 @@
+<footer>
+  <div class="left">
+    <ul>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+    </ul>
+    <ul>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+    </ul>
+    <ul>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+      <li>link</li>
+    </ul>
+  </div>
+  <div class="right">
+    <img src="" alt="">
+  </div>
+</footer>
